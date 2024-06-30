118d41ad873ce21c646d87c6b778f8d49fd0555c
SarahFouda <sarah.fouda2050@gmail.com>
2024-06-30 14:01:40 +0300

    edit links

--- js/main.js
+++ js/main.js
@@ -533,5 +533,19 @@ $('.plus').click(function () {
     $input.change();
     return false;
 });
+var $sticky_nav= $('.secondary_nav');
+	$sticky_nav.find('a').on('click', function(e) {
+		e.preventDefault();
+		var target = this.hash;
+		var $target = $(target);
+		$('html, body').animate({
+			'scrollTop': $target.offset().top - 100
+		}, 800, 'swing');
+	});
+	$sticky_nav.find('ul li a').on('click', function () {
+		$sticky_nav.find('ul li a.active').removeClass('active');
+		$(this).addClass('active');
+	});
+    
 })(jQuery)
 
